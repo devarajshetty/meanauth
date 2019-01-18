d9427bd5192e069a6fdb3b7065c9c9b0b1245b09
devaraj <devaraj@pofitec.com>
2019-01-18 20:24:00 +0530

    'jan18'

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database')
 
+mongoose.connect(config.database);
+mongoose.connection.on('connected',() => {
+    console.log('database connected');
+});
+
+mongoose.connection.on('error',(err) => {
+    console.log('database connected'+err);
+});
 const app = express();
 
 const users =require('./routes/users')
@@ -12,6 +21,10 @@ const users =require('./routes/users')
 const port = 3000;
 //cors
 app.use(cors());
+
+//static folder
+
+app.use(express.static(path.join(__dirname,'public')));
 
 //bodyparser
 app.use(bodyParser.json());
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router =express.Router();
 
+//register
 router.get('/register' ,(req,res,next) => {
     res.send('User Register');
 });
+//authentication
+router.get('/authentication' ,(req,res,next) => {
+    res.send('authentication');
+});
 
+//profile
+router.get('/profile' ,(req,res,next) => {
+    res.send('profile');
+});
+//validate
+router.get('/validate' ,(req,res,next) => {
+    res.send('validate');
+});
 module.exports = router;
