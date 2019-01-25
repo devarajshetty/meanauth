8f72b27559f15d550a32e176fe3917ad1dd656c6
devarajshetty <devaraj.p1403@gmail.com>
2019-01-25 09:01:17 +0530

    part7

--- meanauthapp_part_7/angular-src/src/app/app.module.ts
+++ meanauthapp_part_7/angular-src/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
@@ -12,18 +11,17 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { ValidateService } from './services/validate.service';
+
+import {ValidateService} from './services/validate.service';
 import {AuthService} from './services/auth.service';
 import {FlashMessagesModule} from 'angular2-flash-messages';
-import { HttpModule } from '@angular/http';
 
-
-const appRoutes: Routes = [
-{path: '',component: HomeComponent},
-{path: 'register',component: RegisterComponent},
-{path: 'login',component: LoginComponent},
-{path: 'dashboard',component: DashboardComponent},
-{path: 'profile',component: ProfileComponent}
+const appRoutes: Routes =  [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent},
+  {path:'profile', component: ProfileComponent}
 ]
 
 @NgModule({
@@ -38,11 +36,10 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
+    HttpModule,
     RouterModule.forRoot(appRoutes),
-    FlashMessagesModule.forRoot(),
-    HttpModule
+    FlashMessagesModule
   ],
   providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
